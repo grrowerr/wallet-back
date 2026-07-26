5fbdecf6002c3694f93bb72db3bd592b387b0790
grower <grouw.uzkiy@yandex.ru>
2026-07-26 22:13:02 +0300

    [fix] Настроено подключение к БД

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,18 +1,18 @@
 class DBConfig:
-    DB_USER: str = 'root'
-    DB_PASSWORD: str = ''
+    DB_USER: str = 'postgres'
+    DB_PASSWORD: str = '123qweasd'
     DB_HOST: str = 'localhost'
-    DB_PORT: int = 3306
+    DB_PORT: str = '5432'
     DB_NAME: str = 'wallet_back'
 
     # noinspection pep8-naming,SpellCheckingInspection
     @property
     def DB_URI(self) -> str:
-        db_uri = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}'.format(
+        __db_uri = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}'.format(
             user=self.DB_USER,
             password=self.DB_PASSWORD,
             host=self.DB_HOST,
             port=self.DB_PORT,
             db_name=self.DB_NAME
         )
-        return db_uri
+        return __db_uri
